226ec0aa845c84b9d62326873f646e0a24e9d6ac
Jmmk <jmk525@gmail.com>
2023-11-17 14:41:14 +0900

    feat : 미션 entity 생성

---
+++ src/main/java/umc/study/domain/Mission.java
@@ -0,0 +1,33 @@
+package umc.study.domain;
+import lombok.*;
+import umc.study.domain.common.BaseTimeEntity;
+import umc.study.domain.mapping.MemberMission;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer reward;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissions = new ArrayList<>();
+}
