a159bca567f4f3a08a0fdb6dff0562c18a85c049
Jmmk <jmk525@gmail.com>
2023-11-17 14:42:04 +0900

    feat : 멤버, 미션의 중간 entity 생성

---
+++ src/main/java/umc/study/domain/mapping/MemberMission.java
@@ -0,0 +1,33 @@
+package umc.study.domain.mapping;
+
+import lombok.*;
+import umc.study.domain.Member;
+import umc.study.domain.Mission;
+import umc.study.domain.common.BaseTimeEntity;
+import umc.study.domain.enums.MissionStatus;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MissionStatus missionStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id", nullable = false)
+    private Mission mission;
+}
