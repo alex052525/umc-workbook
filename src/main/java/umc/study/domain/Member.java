c6f6485aadce3e8a5688d11570912bce80c36a35
Jmmk <jmk525@gmail.com>
2023-11-17 14:41:39 +0900

    feat : 멤버 entity 생성

---
+++ src/main/java/umc/study/domain/Member.java
@@ -0,0 +1,52 @@
+package umc.study.domain;
+
+import lombok.*;
+import umc.study.domain.common.BaseTimeEntity;
+import umc.study.domain.enums.FoodCategory;
+import umc.study.domain.enums.Gender;
+import umc.study.domain.enums.MemberStatus;
+import umc.study.domain.mapping.MemberMission;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'", nullable = false)
+    private MemberStatus memberStatus;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private Integer point;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private FoodCategory foodCategoryPreference;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Review> reviews = new ArrayList<>();
+
+}
