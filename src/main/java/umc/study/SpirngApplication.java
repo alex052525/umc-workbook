770275125ca0c670e0f38929c931dcb52d595026
Jmmk <jmk525@gmail.com>
2023-11-17 14:24:12 +0900

    inital commit

---
+++ src/main/java/umc/study/SpirngApplication.java
@@ -0,0 +1,15 @@
+package umc.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpirngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngApplication.class, args);
+	}
+
+}
