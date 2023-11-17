2b6a6cd6bc0ae45ae8c7c2c7e77d898a58aeb06b
Jmmk <jmk525@gmail.com>
2023-11-17 14:39:58 +0900

    feat : 음식 카테고리 enum 생성

---
+++ src/main/java/umc/study/domain/enums/FoodCategory.java
@@ -0,0 +1,26 @@
+package umc.study.domain.enums;
+
+public enum FoodCategory {
+    KOREAN_FOOD("한식"),
+    WESTERN_FOOD("양식"),
+    JAPANESE_FOOD("일식"),
+    CHINESE_FOOD("중식"),
+    CHICKEN("치킨"),
+    SNACKS("분식"),
+    BBQ("고기/구이"),
+    LUNCH_BOX("도시락"),
+    NIGHT_SNACK("야식"),
+    FAST_FOOD("패스트푸드"),
+    DESSERT("디저트"),
+    ASIAN_FOOD("아시안푸드");
+
+    private final String category;
+
+    FoodCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
