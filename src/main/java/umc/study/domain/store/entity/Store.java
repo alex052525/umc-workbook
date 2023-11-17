d0b2ce20c7d90d0fedb40575ca94ef70bca35185
Jmmk <jmk525@gmail.com>
2023-11-17 15:21:33 +0900

    feat : 가게 entity 생성

---
+++ src/main/java/umc/study/domain/store/entity/Store.java
@@ -0,0 +1,37 @@
+package umc.study.domain.store.entity;
+
+import lombok.*;
+import umc.study.domain.region.entity.Region;
+import umc.study.domain.common.entity.BaseTimeEntity;
+import umc.study.domain.review.entity.Review;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String info;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", nullable = false)
+    private Region region;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviews = new ArrayList<>();
+}
