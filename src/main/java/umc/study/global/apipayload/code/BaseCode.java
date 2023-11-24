c5546bbac06e8799b55205e6272f044ed469cb73
Jmmk <jmk525@gmail.com>
2023-11-24 15:36:52 +0900

    feat : 응답 코드 인터페이스 생성

---
+++ src/main/java/umc/study/global/apipayload/code/BaseCode.java
@@ -0,0 +1,9 @@
+package umc.study.global.apipayload.code;
+
+
+
+public interface BaseCode {
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/umc/study/global/apipayload/code/BaseErrorCode.java
@@ -0,0 +1,9 @@
+package umc.study.global.apipayload.code;
+
+
+
+public interface BaseErrorCode {
+    public ErrorReasonDTO getReason();
+
+    public ErrorReasonDTO getReasonHttpStatus();
+}
