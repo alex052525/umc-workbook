b3b82de62305a50909d3de6ef2edf8728435aace
Jmmk <jmk525@gmail.com>
2023-12-01 15:21:02 +0900

    feat : StoreService 생성

---
+++ src/main/java/umc/study/domain/store/service/StoreService.java
@@ -0,0 +1,9 @@
+package umc.study.domain.store.service;
+
+import umc.study.domain.store.dto.StoreRequestDTO;
+import umc.study.domain.store.dto.StoreResponseDTO;
+import umc.study.domain.store.entity.Store;
+
+public interface StoreService {
+    StoreResponseDTO createStore(StoreRequestDTO request);
+}
---
+++ src/main/java/umc/study/domain/store/service/StoreServiceImpl.java
@@ -0,0 +1,29 @@
+package umc.study.domain.store.service;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.StoreManager;
+import org.springframework.stereotype.Service;
+import umc.study.domain.region.entity.Region;
+import umc.study.domain.region.service.RegionService;
+import umc.study.domain.store.converter.StoreConverter;
+import umc.study.domain.store.dto.StoreRequestDTO;
+import umc.study.domain.store.dto.StoreResponseDTO;
+import umc.study.domain.store.entity.Store;
+import umc.study.domain.store.repository.StoreRepository;
+
+@Service
+@RequiredArgsConstructor
+public class StoreServiceImpl implements StoreService{
+    private final StoreRepository storeRepository;
+    private final RegionService regionService;
+    private final StoreConverter storeConverter;
+
+    @Override
+    public StoreResponseDTO createStore(StoreRequestDTO request) {
+        Long regionId = request.getRegion();
+        Region region = regionService.findByRegionId(regionId);
+        Store newStore = storeConverter.toStore(request, region);
+        storeRepository.save(newStore);
+        return storeConverter.toStoreResponseDto(newStore);
+    }
+}
