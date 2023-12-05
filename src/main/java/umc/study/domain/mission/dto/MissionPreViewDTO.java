c673115b7ec7ca47db97b2d7517dd3d1da99e55a
Jmmk <jmk525@gmail.com>
2023-12-05 20:48:16 +0900

    feat : Mission 목록 조회 DTO 생성

---
+++ src/main/java/umc/study/domain/mission/dto/MissionPreViewDTO.java
@@ -0,0 +1,18 @@
+package umc.study.domain.mission.dto;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissionPreViewDTO {
+    String storeName;
+    String description;
+    Integer reward;
+    LocalDate createdAt;
+}
---
+++ src/main/java/umc/study/domain/mission/dto/MissionPreViewListDTO.java
@@ -0,0 +1,21 @@
+package umc.study.domain.mission.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissionPreViewListDTO {
+    List<MissionPreViewDTO> missionList;
+    Integer listSize;
+    Integer totalPage;
+    Long totalElements;
+    Boolean isFirst;
+    Boolean isLast;
+
+}
