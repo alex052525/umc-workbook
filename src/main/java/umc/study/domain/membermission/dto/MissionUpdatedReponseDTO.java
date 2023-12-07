40ac11658788bd46879442bfa4aad33cd0828a84
Jmmk <jmk525@gmail.com>
2023-12-07 17:24:26 +0900

    rename : Mission 업데이트에 대한 응답 하나의 클래스로 통일

--- src/main/java/umc/study/domain/membermission/dto/MissionUpdatedReponseDTO.java
+++ src/main/java/umc/study/domain/membermission/dto/MissionUpdatedReponseDTO.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class MissionChallengeReponseDTO {
+public class MissionUpdatedReponseDTO {
     Long missionId;
     LocalDateTime updateAt;
 }
